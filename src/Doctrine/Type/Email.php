f9def716e800be26c547afdd946d21c6d939e2b2
Karel Horsky <karel.horsky@centrum.cz>
2017-07-14 13:26:58 +0200

    add custom Doctrine types for Email, Url

---
+++ src/Doctrine/Type/Email.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace AipNg\ValueObjects\Doctrine\Type;
+
+use AipNg\ValueObjects\Web\Email as EmailObject;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+final class Email extends Type
+{
+
+	private const NAME = 'email';
+
+
+	/**
+	 * @param mixed[] $fieldDeclaration
+	 * @param \Doctrine\DBAL\Platforms\AbstractPlatform $platform
+	 *
+	 * @return string
+	 */
+	public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform): string
+	{
+		return $platform->getVarcharTypeDeclarationSQL($fieldDeclaration);
+	}
+
+
+	public function getName(): string
+	{
+		return self::NAME;
+	}
+
+
+	/**
+	 * @param mixed|\AipNg\ValueObjects\Web\Email $value
+	 * @param \Doctrine\DBAL\Platforms\AbstractPlatform $platform
+	 *
+	 * @return string
+	 */
+	public function convertToDatabaseValue($value, AbstractPlatform $platform): string
+	{
+		return $value->getValue();
+	}
+
+
+	/**
+	 * @param mixed|string $value
+	 * @param \Doctrine\DBAL\Platforms\AbstractPlatform $platform
+	 *
+	 * @return \AipNg\ValueObjects\Web\Email
+	 *
+	 * @throws \AipNg\ValueObjects\InvalidArgumentException
+	 */
+	public function convertToPHPValue($value, AbstractPlatform $platform): EmailObject
+	{
+		return new EmailObject($value);
+	}
+
+}
---
+++ src/Doctrine/Type/Url.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace AipNg\ValueObjects\Doctrine\Type;
+
+use AipNg\ValueObjects\Web\Url as UrlObject;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+final class Url extends Type
+{
+
+	private const NAME = 'url';
+
+
+	/**
+	 * @param mixed[] $fieldDeclaration
+	 * @param \Doctrine\DBAL\Platforms\AbstractPlatform $platform
+	 *
+	 * @return string
+	 */
+	public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform): string
+	{
+		return $platform->getVarcharTypeDeclarationSQL($fieldDeclaration);
+	}
+
+
+	public function getName(): string
+	{
+		return self::NAME;
+	}
+
+
+	/**
+	 * @param mixed|\AipNg\ValueObjects\Web\Url $value
+	 * @param \Doctrine\DBAL\Platforms\AbstractPlatform $platform
+	 *
+	 * @return string
+	 */
+	public function convertToDatabaseValue($value, AbstractPlatform $platform): string
+	{
+		return $value->getValue();
+	}
+
+
+	/**
+	 * @param mixed|string $value
+	 * @param \Doctrine\DBAL\Platforms\AbstractPlatform $platform
+	 *
+	 * @return \AipNg\ValueObjects\Web\Url
+	 *
+	 * @throws \AipNg\ValueObjects\InvalidArgumentException
+	 */
+	public function convertToPHPValue($value, AbstractPlatform $platform): UrlObject
+	{
+		return new UrlObject($value);
+	}
+
+}
