f4c66c2fa904d7e962595cfffca639d52afa4e8a
Karel Horsky <karel.horsky@centrum.cz>
2018-10-08 20:33:47 +0200

    introduce Exception marker interface

---
+++ src/Exception.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace AipNg\ValueObjects;
+
+interface Exception
+{
+
+}
--- src/InvalidArgumentException.php
+++ src/InvalidArgumentException.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 
 namespace AipNg\ValueObjects;
 
-class InvalidArgumentException extends \InvalidArgumentException
+class InvalidArgumentException extends \InvalidArgumentException implements Exception
 {
 
 }
