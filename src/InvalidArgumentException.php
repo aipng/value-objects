e67da1cd83b0e63830122927961183406fdf3023
Karel Horsky <karel.horsky@centrum.cz>
2018-10-10 10:08:28 +0200

    revert 2e1e09c - inherit from \InvalidArgumentException again

--- src/InvalidArgumentException.php
+++ src/InvalidArgumentException.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 
 namespace AipNg\ValueObjects;
 
-final class InvalidArgumentException extends \Exception implements Exception
+final class InvalidArgumentException extends \InvalidArgumentException implements Exception
 {
 
 }
